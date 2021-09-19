697f40f52179b98fa60b25e076b768eaf877448a
adavidarevalo <davidarevaloc20@gmail.com>
2021-09-18 22:19:29 -0500

    change the information

--- information/information.js
+++ information/information.js
@@ -1,19 +1,31 @@
 exports.Planet =[ 
   {
     name: "mercury",
-    main:{
-      rotacionTime: "58.6",
-      revolutionTime: "87.97",
-      radius: "2,439.7",
-      temp: "430",    
-      source: 'https://en.wikipedia.org/wiki/Mercury_(planet)'  
-    },
+    source: 'https://en.wikipedia.org/wiki/Mercury_(planet)',  
+    main:[
+      {
+        name: 'rotacionTime',
+        data: '58.6'
+      },
+      {
+        name: 'revolutionTime',
+        data: '87.97'
+      },
+      {
+        name: 'radius',
+        data: '2,439.7'
+      },
+      {
+        name: 'temp',
+        data: '430'
+      },
+    ],
     overview: {
-      image: "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='290' height='290'><defs><circle id='a' cx='145' cy='145' r='145'/><circle id='c' cx='145' cy='145' r='145'/></defs><g fill='none' fill-rule='evenodd'><circle cx='145' cy='145' r='145' fill='#DEF4FC' fill-rule='nonzero'/><mask id='b' fill='#fff'><use xlink:href='#a'/></mask><path fill='#B1D5E2' fill-rule='nonzero' d='M154 253c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm86-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-62c-6.627 0-12-5.373-12-12s5.373-12 12-12h62zm-110 0c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-45-60c0-6.627-5.373-12-12-12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h49c6.627 0 12-5.373 12-12s-5.373-12-12-12h-5c-6.627 0-12-5.373-12-12s5.373-12 12-12h73c6.627 0 12 5.373 12 12s-5.373 12-12 12H99v.01c-6.395.262-11.5 5.53-11.5 11.99s5.105 11.728 11.5 11.99v.01h45c6.627 0 12 5.373 12 12s-5.373 12-12 12h-2l-.001.01c-6.395.263-11.499 5.53-11.499 11.99s5.104 11.727 11.499 11.99l.001.01h52c6.627 0 12 5.373 12 12s-5.373 12-12 12H70c-6.627 0-12-5.373-12-12s5.373-12 12-12h3c6.627 0 12-5.373 12-12zm160 12c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-223 0c6.627 0 12 5.373 12 12s-5.373 12-12 12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm199-96c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm24-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-58c-6.627 0-12-5.373-12-12s5.373-12 12-12h58z' mask='url(#b)'/><mask id='d' fill='#fff'><use xlink:href='#c'/></mask><path fill='#000' fill-rule='nonzero' mask='url(#d)' opacity='.078' d='M146 0h145v290H146z'/></g></svg>",
+      image: 'https://i.ibb.co/bdHQk76/planet-mercury.png',
       description: 'Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun is planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.'
     },
     structure: {
-      image: "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='290' height='290'><defs><circle id='a' cx='145' cy='145' r='145'/><circle id='c' cx='145' cy='145' r='145'/><path id='e' d='M0 0c76.768 0 139 62.232 139 139S76.768 278 0 278z'/></defs><g fill='none' fill-rule='evenodd'><circle cx='145' cy='145' r='145' fill='#DEF4FC' fill-rule='nonzero'/><mask id='b' fill='#fff'><use xlink:href='#a'/></mask><path fill='#B1D5E2' fill-rule='nonzero' d='M154 253c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm86-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-62c-6.627 0-12-5.373-12-12s5.373-12 12-12h62zm-110 0c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-45-60c0-6.627-5.373-12-12-12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h49c6.627 0 12-5.373 12-12s-5.373-12-12-12h-5c-6.627 0-12-5.373-12-12s5.373-12 12-12h73c6.627 0 12 5.373 12 12s-5.373 12-12 12H99v.01c-6.395.262-11.5 5.53-11.5 11.99s5.105 11.728 11.5 11.99v.01h45c6.627 0 12 5.373 12 12s-5.373 12-12 12h-2l-.001.01c-6.395.263-11.499 5.53-11.499 11.99s5.104 11.727 11.499 11.99l.001.01h52c6.627 0 12 5.373 12 12s-5.373 12-12 12H70c-6.627 0-12-5.373-12-12s5.373-12 12-12h3c6.627 0 12-5.373 12-12zm160 12c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-223 0c6.627 0 12 5.373 12 12s-5.373 12-12 12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm199-96c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm24-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-58c-6.627 0-12-5.373-12-12s5.373-12 12-12h58z' mask='url(#b)'/><mask id='d' fill='#fff'><use xlink:href='#c'/></mask><path fill='#000' fill-rule='nonzero' mask='url(#d)' opacity='.078' d='M146 0h145v290H146z'/><g transform='translate(146 6)'><mask id='f' fill='#fff'><use xlink:href='#e'/></mask><use fill='#7A939C' xlink:href='#e'/><circle cx='1' cy='140' r='131' fill='#9AB2BB' mask='url(#f)'/><circle cx='1' cy='139' r='95' fill='#E8903F' mask='url(#f)'/><circle cx='.5' cy='139.5' r='41.5' fill='#FFEA87' mask='url(#f)'/></g></g></svg>",
+      image: 'https://i.ibb.co/4gTry19/planet-mercury-internal.png',
       description: 'Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet is density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth is density.'
     },
     surface: {
