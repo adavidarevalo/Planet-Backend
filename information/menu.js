dff999a1dd8db0f5d8589a76afdf74f8df04d9d0
David Arevalo <davidarevaloc20@gmail.com>
2021-09-18 21:54:29 -0500

    change the menu

--- information/menu.js
+++ information/menu.js
@@ -1,22 +1,17 @@
 exports.information = [
     {
-        color: '#DEF4FC',
         name: 'mercury'
     },
     {
-        color: '#F7CC7F',
         name: 'venus'
     },
     {
-        color: '#545BFE',
         name: 'earth'
     },
     {
-        color: '#FF6A45',
         name: 'mars'
     },
     {
-        color: '#ECAD7A',
         name: 'jupiter'
     },
     {
