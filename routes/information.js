413284aced9aeff8089a14dc49886b0759ab0e49
David Arevalo <davidarevaloc20@gmail.com>
2021-09-18 19:27:09 -0500

    resolve the bug

--- routes/information.js
+++ routes/information.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const imformationController  = require("../controllers/informationController")
 
-router.get("/",
+router.put("/:name",
     imformationController.allData
 );
 
