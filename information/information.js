4b9d1d393e89a8353e878c6114a1a6ccab4998d8
David Arevalo <davidarevaloc20@gmail.com>
2021-09-20 12:44:12 -0500

    Added all information

--- information/information.js
+++ information/information.js
@@ -21,7 +21,7 @@ exports.Planet =[
       },
     ],
     overview: {
-      image: 'https://i.ibb.co/bdHQk76/planet-mercury.png',
+      image: "https://i.ibb.co/17frFvp/planet-mercury.png",
       description: 'Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun is planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.'
     },
     structure: {
@@ -34,103 +34,244 @@ exports.Planet =[
     }
   },
   {
-    name: "mars",
-    description: "loren"
-  },  
-  {
     name: "venus",
-    description: "loren"
-  }
-  ]
-
-
-const Planets = [
-  {
-    name: "mercury",
-    description: "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
-    rotacionTime: "58.6",
-    revolutionTime: "87.97",
-    radius: "2,439.7",
-    temp: "430",
-    imageOverview: __dirname + "/../assets/" + 'geology-earth.png',
-    imageStructure: "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='290' height='290'><defs><circle id='a' cx='145' cy='145' r='145'/><circle id='c' cx='145' cy='145' r='145'/></defs><g fill='none' fill-rule='evenodd'><circle cx='145' cy='145' r='145' fill='#DEF4FC' fill-rule='nonzero'/><mask id='b' fill='#fff'><use xlink:href='#a'/></mask><path fill='#B1D5E2' fill-rule='nonzero' d='M154 253c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm86-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-62c-6.627 0-12-5.373-12-12s5.373-12 12-12h62zm-110 0c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-45-60c0-6.627-5.373-12-12-12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h49c6.627 0 12-5.373 12-12s-5.373-12-12-12h-5c-6.627 0-12-5.373-12-12s5.373-12 12-12h73c6.627 0 12 5.373 12 12s-5.373 12-12 12H99v.01c-6.395.262-11.5 5.53-11.5 11.99s5.105 11.728 11.5 11.99v.01h45c6.627 0 12 5.373 12 12s-5.373 12-12 12h-2l-.001.01c-6.395.263-11.499 5.53-11.499 11.99s5.104 11.727 11.499 11.99l.001.01h52c6.627 0 12 5.373 12 12s-5.373 12-12 12H70c-6.627 0-12-5.373-12-12s5.373-12 12-12h3c6.627 0 12-5.373 12-12zm160 12c6.627 0 12 5.373 12 12s-5.373 12-12 12-12-5.373-12-12 5.373-12 12-12zm-223 0c6.627 0 12 5.373 12 12s-5.373 12-12 12H2c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm199-96c6.627 0 12 5.373 12 12s-5.373 12-12 12h-20c-6.627 0-12-5.373-12-12s5.373-12 12-12h20zm24-48c6.627 0 12 5.373 12 12s-5.373 12-12 12h-58c-6.627 0-12-5.373-12-12s5.373-12 12-12h58z' mask='url(#b)'/><mask id='d' fill='#fff'><use xlink:href='#c'/></mask><path fill='#000' fill-rule='nonzero' mask='url(#d)' opacity='.078' d='M146 0h145v290H146z'/></g></svg>",
-    imageGeology: "",
-  },
-  {
-    name: "venus",
-    description: "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.",
-    rotacionTime: "243",
-    revolutionTime: "224.7",
-    radius: "6,051.8",
-    temp: "471",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Venus',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '243 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '224.7 Days'
+      },
+      {
+        name: 'radius',
+        data: '6,051.8 Km'
+      },
+      {
+        name: 'temp',
+        data: '471 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/kcPRS27/planet-venus.png",
+      description: 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth is night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/wKmh6td/planet-venus-internal.png',
+      description: 'The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.'
+    },
+    surface: {
+      image: "https://i.ibb.co/sgYnK2D/geology-venus.png",
+      description: 'Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.'
+    }
   },
   {
     name: "earth",
-    description: "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.",
-    rotacionTime: "0.99",
-    revolutionTime: "365.26",
-    radius: "6,371",
-    temp: "16",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Earth',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '0.99 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '365.26 Days'
+      },
+      {
+        name: 'radius',
+        data: '6,371 Km'
+      },
+      {
+        name: 'temp',
+        data: '16 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/3CTtXnK/planet-earth.png",
+      description: 'Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth is surface is land with remaining 70.8% is covered with water. Earth is distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/P92c8GS/planet-earth-internal.png',
+      description: 'Earth is interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.'
+    },
+    surface: {
+      image: "https://i.ibb.co/pWP13Hq/geology-earth.png",
+      description: 'The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.'
+    }
   },
   {
     name: "mars",
-    description: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the Red Planet",
-    rotacionTime: "1.03",
-    revolutionTime: "1.88",
-    radius: "3,389.5 ",
-    temp: "-28",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Mars',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '1.03 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '1.88 Days'
+      },
+      {
+        name: 'radius',
+        data: '3,389.5 Km'
+      },
+      {
+        name: 'temp',
+        data: '-28 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/GMkjhxp/planet-mars.png",
+      description: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".'
+    },
+    structure: {
+      image: 'https://i.ibb.co/C9NzpPz/planet-mars-internal.png',
+      description: 'Like Earth, Mars has differentiated into a dense metallic core overlaid by less dense materials. Scientists initially determined that the core is at least partially liquid. Current models of its interior imply a core consisting primarily of iron and nickel with about 16–17% sulfur.'
+    },
+    surface: {
+      image: "https://i.ibb.co/BNyJZjb/geology-mars.png",
+      description: 'Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt.'
+    }
   },
   {
     name: "jupiter",
-    description: "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.",
-    rotacionTime: "9.93",
-    revolutionTime: "11.86",
-    radius: "369,911",
-    temp: "-108",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Jupiter',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '9.93 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '11.86 Years'
+      },
+      {
+        name: 'radius',
+        data: '69,911 Km'
+      },
+      {
+        name: 'temp',
+        data: '-108 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/ZYF8xLy/planet-jupiter.png",
+      description: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/P6Wvxn4/planet-jupiter-internal.png',
+      description: 'When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter is formation, which would have disrupted an originally solid Jovian core.'
+    },
+    surface: {
+      image: "https://i.ibb.co/thNbFQG/geology-jupiter.png",
+      description: 'The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665.'
+    }
   },
   {
     name: "saturn",
-    description: "Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth.",
-    rotacionTime: "10.8",
-    revolutionTime: "29.46",
-    radius: "58,232",
-    temp: "-138",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Saturn',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '10.8 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '29.46 Years'
+      },
+      {
+        name: 'radius',
+        data: '58,232 Km'
+      },
+      {
+        name: 'temp',
+        data: '-138 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/ZV56h50/planet-saturn.png",
+      description: 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/G7ZxtWd/planet-saturn-internal.png',
+      description: 'Despite consisting mostly of hydrogen and helium, most of Saturn is mass is not in the gas phase, because hydrogen becomes a non-ideal liquid when the density is above 0.01 g/cm3, which is reached at a radius containing 99.9% of Saturn is mass.'
+    },
+    surface: {
+      image: "https://i.ibb.co/KDHgF1s/geology-saturn.png",
+      description: 'The outer atmosphere of Saturn contains 96.3% molecular hydrogen and 3.25% helium by volume. The planet is most famous feature is its prominent ring system, which is composed mostly of ice particles with a smaller amount of rocky debris and dust.'
+    }
   },
   {
     name: "uranus",
-    description: "Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.",
-    rotacionTime: "17.2 ",
-    revolutionTime: "84",
-    radius: "25,362",
-    temp: "-195",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Uranus',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '17.2 Days'
+      },
+      {
+        name: 'revolution Time',
+        data: '84 Years'
+      },
+      {
+        name: 'radius',
+        data: '25,362 Km'
+      },
+      {
+        name: 'temp',
+        data: '-195 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/st4zcDg/planet-uranus.png",
+      description: 'Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/wJ29fmH/planet-uranus-internal.png',
+      description: 'The standard model of Uranus is structure is that it consists of three layers: a rocky (silicate/iron–nickel) core in the centre, an icy mantle in the middle and an outer gaseous hydrogen/helium envelope. The core is relatively small, with a mass of only 0.55 Earth masses.'
+    },
+    surface: {
+      image: "https://i.ibb.co/YBw0Hch/geology-uranus.png",
+      description: 'The composition of Uranus is atmosphere is different from its bulk, consisting mainly of molecular hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is 0.15±0.03 in the upper troposphere.'
+    }
   },
   {
     name: "neptune",
-    description: "Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, more massive than its near-twin Uranus.",
-    rotacionTime: "16.08",
-    revolutionTime: "164.79",
-    radius: "24,622",
-    temp: "-201",
-    imageOverview: "",
-    imageStructure: "",
-    imageGeology: "",
+    source: 'https://en.wikipedia.org/wiki/Neptune',  
+    main:[
+      {
+        name: 'rotacion Time',
+        data: '16.08 Hours'
+      },
+      {
+        name: 'revolution Time',
+        data: '164.79 Years'
+      },
+      {
+        name: 'radius',
+        data: '24,622 Km'
+      },
+      {
+        name: 'temp',
+        data: '-201 °C'
+      },
+    ],
+    overview: {
+      image: "https://i.ibb.co/2k9Qnx5/planet-neptune.png",
+      description: 'Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, more massive than its near-twin Uranus.'
+    },
+    structure: {
+      image: 'https://i.ibb.co/0jg8YRn/planet-neptune-internal.png',
+      description: 'Neptune is internal structure resembles that of Uranus. Its atmosphere forms about 5% to 10% of its mass and extends perhaps 10% to 20% of the way towards the core. Increasing concentrations of methane, ammonia and water are found in the lower regions.'
+    },
+    surface: {
+      image: "https://i.ibb.co/gMGBSwb/geology-neptune.png",
+      description: 'Neptune is atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the spectrum.'
+    }
   },
+
 ]
+
+
